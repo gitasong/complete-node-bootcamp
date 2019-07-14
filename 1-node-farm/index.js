ea67e8d6a23664cb778bda56f3f7991ef13b2748
Nicole F <nicole.freed@gmail.com>
2019-07-14 15:36:38 -0700

    comment out previous code & add section headings

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+////////////////////////////////
+// FILES
 // Blocking, synchronous method
 // const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -9,18 +11,21 @@ const fs = require('fs');
 // console.log('File written.');
 
 // Non-blocking, asynchronous method
-fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('Error! :(');
+// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('Error! :(');
+//
+//   fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
+//
+//       fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('File has been written! :)');
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file!');
 
-  fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3);
-
-      fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('File has been written! :)');
-      });
-    });
-  });
-});
-console.log('Will read file!');
+////////////////////////////////
+// SERVER
