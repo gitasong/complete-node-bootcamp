bdae7ed85dee65b527f4c9d28188b62c5872e6f9
Nicole F <nicole.freed@gmail.com>
2019-07-14 15:27:40 -0700

    add third async function + callback

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -14,6 +14,10 @@ fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
     console.log(data2);
     fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
       console.log(data3);
+
+      fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+        console.log('File has been written! :)');
+      });
     });
   });
 });
