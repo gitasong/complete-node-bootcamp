e425b5311fab495e5d6b0c0c1bd59d5c71e1084d
Nicole F <nicole.freed@gmail.com>
2019-07-14 13:40:57 -0700

    add async version; comment out syn version

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,15 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
-console.log(textIn);
+// Blocking, synchronous method
+// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
+// console.log(textIn);
+//
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}.`;
+// fs.writeFileSync('./starter/txt/output.txt', textOut);
+// console.log('File written.');
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}.`;
-fs.writeFileSync('./starter/txt/output.txt', textOut);
-console.log('File written.');
+// Non-blocking, asynchronous method
+fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data) => {
+  console.log(data);
+});
+console.log('Will read file!');
